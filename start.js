a063911ed890f77b4cf5d28d9da1fde56529bd4d
Jordan Hall <jordan.hall@rapidweb.biz>
2016-01-05 11:05:55 +0000

    Removed fullscreen and switch to maximized window

--- start.js
+++ start.js
@@ -21,16 +21,19 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', function() {
-    
+
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 1000, height: 700, icon: 'g', fullscreen: true});
-    
+    mainWindow = new BrowserWindow({width: 1000, height: 700, icon: 'g', fullscreen: false});
+
+    // Maximize
+    mainWindow.maximize();
+
     // and load the index.html of the app.
     mainWindow.loadUrl('file://' + __dirname + '/index.html');
-    
+
     // Open the devtools.
     //mainWindow.openDevTools();
-    
+
     // Emitted when the window is closed.
     mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
